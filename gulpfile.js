09a5e0e8b5f9dce4b121e69284e536431d693ea7
ygorkagrishin <grishingrishin@yahoo.com>
2020-04-29 18:47:14 +0300

    Заменено правило в pug-lintrc: validateAttributeQuoteMarks: ",

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-const { src, dest, watch, series, parallel, lastRun } = require('gulp')
-const $ = require('gulp-load-plugins')()
-const browserSync = require('browser-sync').create()
-const del = require('del')
+const { src, dest, watch, series, parallel, lastRun } = require('gulp');
+const $ = require('gulp-load-plugins')();
+const browserSync = require('browser-sync').create();
+const del = require('del');
 
-$.sass.compiler = require('node-sass')
+$.sass.compiler = require('node-sass');
 
-const port = process.env.PORT || 9000
-const isProd = process.env.NODE_ENV === 'production'
+const port = process.env.PORT || 9000;
+const isProd = process.env.NODE_ENV === 'production';
 
 function clean() {
-  return del(['.tmp', 'dist'])
+  return del(['.tmp', 'dist']);
 }
 
 function html() {
@@ -19,23 +19,23 @@ function html() {
         $.notify.onError({
           title: 'Error on pug',
           message: err.message
-        })(err)
+        })(err);
       }
     }))
-    .pipe($.pugLinter({ reporter: 'default' }))
+    .pipe($.pugLinter({ failAfterError: true }))
     .pipe($.pug({ pretty: true }))
     .pipe($.if(isProd, dest('dist'), dest('.tmp')))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
-function styles() { 
+function styles() {
   return src('app/styles/**/*.scss')
     .pipe($.plumber({
       errorHandler: err => {
         $.notify.onError({
           title: 'Error on scss',
           message: err.message
-        })(err)
+        })(err);
       }
     }))
     .pipe($.if(!isProd, $.sourcemaps.init()))
@@ -43,7 +43,7 @@ function styles() {
     .pipe($.autoprefixer())
     .pipe($.if(!isProd, $.sourcemaps.write()))
     .pipe($.if(!isProd, dest('.tmp/styles'), dest('dist/styles')))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function scripts() {
@@ -53,7 +53,7 @@ function scripts() {
         $.notify.onError({
           title: 'Error on js',
           message: err.message
-        })(err)
+        })(err);
       }
     }))
     .pipe($.if(!isProd, $.sourcemaps.init()))
@@ -62,23 +62,23 @@ function scripts() {
     // .pipe($.if(!isProd, $.uglify()))
     .pipe($.if(!isProd, $.sourcemaps.write()))
     .pipe($.if(!isProd, dest('.tmp/scripts'), dest('dist/scripts')))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function fonts() {
   return src('app/fonts/**/*.{eot,svg,ttf,woff,woff2}')
-    .pipe($.if(!isProd, dest('.tmp/fonts'), dest('dist/fonts')))
+    .pipe($.if(!isProd, dest('.tmp/fonts'), dest('dist/fonts')));
 }
 
 function images() {
   return src('app/images/**/*.{png,jpg}', { since: lastRun(images) })
-    .pipe($.if(!isProd, dest('.tmp/images'), dest('dist/images')))
+    .pipe($.if(!isProd, dest('.tmp/images'), dest('dist/images')));
 }
 
 function useref() {
   return src('dist/**/*.{html,scss,js}')
     .pipe($.replace('/node_modules', 'node_modules'))
-    .pipe($.useref({ searchPath: [ 'dist', '.' ] }))
+    .pipe($.useref({ searchPath: ['dist', '.'] }))
     .pipe(dest('dist'));
 }
 
@@ -92,20 +92,20 @@ const server = () => {
         '/node_modules': 'node_modules'
       }
     }
-  })
+  });
 
   watch([
     'app/images/**/*',
     'app/fonts/**/*'
-  ]).on('change', browserSync.reload)
+  ]).on('change', browserSync.reload);
 
-  watch('app/pages/**/*.pug', html)
-  watch('app/styles/**/*.scss', styles)
-  watch('app/scripts/**/*.js', scripts)
-}
+  watch('app/pages/**/*.pug', html);
+  watch('app/styles/**/*.scss', styles);
+  watch('app/scripts/**/*.js', scripts);
+};
 
-const build = series(clean, fonts, images, parallel(styles, scripts), html)
+const build = series(clean, fonts, images, parallel(styles, scripts), html);
 
-exports.build = series(build, useref)
+exports.build = series(build, useref);
 
-exports.default = series(build, server)
+exports.default = series(build, server);
